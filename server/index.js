4b00eb24ebd224c56ad066a28fc5a0c017c958e2
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-24 01:23:53 +0000

    Fixes port env mispelling.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const PORT = proces.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
